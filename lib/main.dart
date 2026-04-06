e091a376863ce8617be297bb48a4dcc397a1475a
Phsithzz <phoosith2548@gmail.com>
2026-04-06 13:56:43 +0700

    study: popupmenuButton

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc_study/screens/home_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,10 +10,60 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: Text("Simple Menu"),
+          actions: [
+            PopupMenuButton(
+              onSelected: (value) {
+                print(value);
+              },
+              itemBuilder: (context) {
+                return <PopupMenuEntry>[
+                  PopupMenuItem(value: "1",
+                   child: Text("Menu One")),
+                  PopupMenuDivider(),
+                  PopupMenuItem(value: "2", 
+                  child: Text("Menu Two")),
+                ];
+              },
+            ),
+          ],
+        ),
+        body: Center(child: Text("Flutter Simple Menu")),
+        floatingActionButton: Builder(
+          builder: (context) {
+            return FloatingActionButton(
+              onPressed: () {
+                print("pressed");
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return Container(
+                      color: Colors.amber,
+                      child: Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text("Hello Dialog"),
+                            ElevatedButton(
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                              child: Text("OK"),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                );
+              },
+              child: Icon(Icons.add),
+            );
+          },
         ),
       ),
     );
